522004fdd2fa09e157b174d065ecbf996115f519
Alejandro Montoya <alejodosmildosarroba@gmail.com>
2024-04-19 18:53:10 -0500

    tratando de cuadrar esto

---
+++ ejercicio4.js
@@ -0,0 +1,3 @@
+var nombre = prompt("Por favor ingrese su nombre");
+var nacimiento = prompt("Por favor ingrese su año de nacimiento");
+console.log(`!Hola!  ${nombre} , tienes ${nacimiento} años`);
