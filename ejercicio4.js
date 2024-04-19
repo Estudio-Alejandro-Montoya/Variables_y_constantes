6b316aa80103b3610c486ca53c1b62396b2b5699
Alejandro Montoya <alejodosmildosarroba@gmail.com>
2024-04-19 18:58:11 -0500

    Cuadrando esto v2

---
+++ ejercicio4.js
@@ -0,0 +1,5 @@
+let user_name = prompt("ingrese su nombre");
+let birthYear = parseInt(prompt("ingrese su año de nacimiento"));
+currentYear = 2024;
+var Añospersona = currentYear - birthYear;
+console.log (" Hola " + user_name + ", tienes "+ Añospersona + " años ");
